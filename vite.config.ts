1e1149369458d445769f18d138c7fac3cc670d9b
Mibrahim-hub <it.systemadmin91@gmail.com>
2026-08-08 02:51:16 +0300

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,18 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Portfolio/',
-
   plugins: [react()],
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+
+  base: '/Portfolio1/',
 })
+
+
+
+
+
